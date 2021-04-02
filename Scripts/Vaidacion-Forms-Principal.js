d37e9cca920237be915110109f4949a132644f77
Cristhian <ada_garcia1992@hotmail.com>
2021-04-02 00:17:13 -0600

    Cambio de forms

--- Scripts/Vaidacion-Forms-Principal.js
+++ Scripts/Vaidacion-Forms-Principal.js
@@ -7,9 +7,9 @@ function valida_envia()
         return 0; 
     }
 
-    if(document.getElementById("No"))
+    if(document.getElementById("No").checked)
         window.location.replace('Forms.html');
-    if(document.getElementById("Si"))
+    if(document.getElementById("Si").checked)
         window.location.replace('Forms-Secundario.html');
 }   
 
